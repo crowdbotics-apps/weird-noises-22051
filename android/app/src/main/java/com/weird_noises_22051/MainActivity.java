9b88d6094214c3c285fc77a928ce6f158f4a3616
Crowdbotics <team@crowdbotics.com>
2020-10-28 12:40:38 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/weird_noises_22051/MainActivity.java
@@ -0,0 +1,15 @@
+package com.weird_noises_22051;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "weird_noises_22051";
+    }
+}
